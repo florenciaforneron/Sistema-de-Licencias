7e1b2a4363a3a07b1c899def6ade86bd91604c83
MRX <48726203+5korpi0n@users.noreply.github.com>
2020-01-13 09:51:05 -0300

    Creacion de Sistema de Licencia

---
+++ sistemaDeLicencia/src/main/java/com/cdainfo/sistemaDeLicencia/SistemaDeLicenciaApplication.java
@@ -0,0 +1,13 @@
+package com.cdainfo.sistemaDeLicencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeLicenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeLicenciaApplication.class, args);
+	}
+
+}
---
+++ sistemaDeLicencia/src/test/java/com/cdainfo/sistemaDeLicencia/SistemaDeLicenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdainfo.sistemaDeLicencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeLicenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
